3e9751d913fa086796090e6c6be68883f3c85715
irsan Mansyur <irsan00mansyur@gmail.com>
2019-11-25 20:57:12 +0200

    new db backup zip

---
+++ application/controllers/Backup.php
@@ -0,0 +1,49 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Backup extends CI_Controller
+{
+
+    // backup files in directory
+    function files()
+    {
+        $opt = array(
+            'src' => FCPATH . "assets/", // dir name to backup
+            'dst' => 'backup/files' // dir name backup output destination
+        );
+
+        // Codeigniter v3x
+        $this->load->library('RecurseZip', $opt);
+        // $this->RecurseZip($opt);
+        // $this->RecurseZip->setOPT($opt);
+        $download = $this->recursezip->compress();
+
+        /* Codeigniter v2x
+     $zip    = $this->load->library('RecurseZip', $opt);     
+     $download = $zip->compress();
+     */
+
+        redirect(base_url($download));
+    }
+
+    // backup database.sql
+    public function db()
+    {
+        $this->load->dbutil();
+
+        $prefs = array(
+            'format' => 'zip',
+            'filename' => 'my_db_backup.sql'
+        );
+
+        $backup = $this->dbutil->backup($prefs);
+
+        $db_name = 'backup-on-' . date("Y-m-d-H-i-s") . '.zip'; // file name
+        $save  = APPPATH . 'backup/db/' . $db_name; // dir name backup output destination
+
+        $this->load->helper('file');
+
+        write_file($save, $backup);
+        $this->load->helper('download');
+        force_download($db_name, $backup);
+    }
+}
